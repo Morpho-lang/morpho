05ec7ad4080589c752c96188590de26e49ce0089
softmattertheory <timothy.atherton@tufts.edu>
2022-11-06 11:52:37 -0700

    Separated out profiler code

---
+++ morpho5/vm/profile.h
@@ -0,0 +1,15 @@
+/** @file profile.h
+ *  @author T J Atherton
+ *
+ *  @brief Profiler
+ */
+
+#ifndef profile_h
+#define profile_h
+
+#include "compile.h"
+#include "vm.h"
+#include "profile.h"
+#include "morpho.h"
+
+#endif /* profile_h */

19db60c2f1c646e1678d4083f189b1bde0783c5d
softmattertheory <timothy.atherton@tufts.edu>
2024-12-27 11:37:00 -0500

    Empty module

---
+++ src/newlinalg.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <morpho.h>
+#include <classes.h>
+
+#include "newlinalg.h"
+
+/* -------------------------------------------------------
+ * Initialization and finalization
+ * ------------------------------------------------------- */
+
+void newlinalg_initialize(void) { 
+}
+
+void newlinalg_finalize(void) { 
+}
---
+++ src/newlinalg.h
@@ -0,0 +1,11 @@
+
+/** @file newlinalg.h
+ *  @author T J Atherton
+ *
+ *  @brief New linear algebra library
+*/
+
+#ifndef newlinalg_h
+#define newlinalg_h
+
+#endif

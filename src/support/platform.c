188dfb35c4d516abd7861f1bb576b706606b0f8a
Tim Atherton <54725421+softmattertheory@users.noreply.github.com>
2025-01-18 07:43:56 -0500

    Create platform .c/.h to isolate platform dependence

---
+++ src/support/platform.c
@@ -0,0 +1,6 @@
+/** @file platform.c
+ *  @author T J Atherton 
+ *
+ *  @brief Isolates platform dependent code in morpho
+*/
+
---
+++ src/support/platform.h
@@ -0,0 +1,6 @@
+/** @file platform.h
+ *  @author T J Atherton 
+ *
+ *  @brief Provide an interface to isolate platform dependent code in morpho
+*/
+

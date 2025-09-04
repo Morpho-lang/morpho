89d734d209b9e65fae241365635f758876210956
softmattertheory <timothy.atherton@tufts.edu>
2025-09-04 09:01:51 -0400

    Add placeholders for help

---
+++ src/support/help.c
@@ -0,0 +1,10 @@
+/** @file help.c
+ *  @author T J Atherton
+ *
+ *  @brief Morpho help
+*/
+
+#include <string.h>
+#include <ctype.h>
+
+#include "help.h"
---
+++ src/support/help.h
@@ -0,0 +1,10 @@
+/** @file help.h
+ *  @author T J Atherton and others (see below)
+ *
+ *  @brief Morpho help
+*/
+
+#ifndef help_h
+#define help_h
+
+#endif /* help_h */

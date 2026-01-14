3bb2a01f36544fb3963ee6199cc7c39ea4875c9b
Tim Atherton <54725421+softmattertheory@users.noreply.github.com>
2026-01-14 10:35:08 -0500

    Move files into subfolder to facilitate merge

---
+++ newlinalg/build/CMakeFiles/newlinalg.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/timatherton/Documents/Programs/packages/morpho-newlinalg/src/newlinalg.c" "CMakeFiles/newlinalg.dir/src/newlinalg.c.o" "gcc" "CMakeFiles/newlinalg.dir/src/newlinalg.c.o.d"
+  "/Users/timatherton/Documents/Programs/packages/morpho-newlinalg/src/xcomplexmatrix.c" "CMakeFiles/newlinalg.dir/src/xcomplexmatrix.c.o" "gcc" "CMakeFiles/newlinalg.dir/src/xcomplexmatrix.c.o.d"
+  "/Users/timatherton/Documents/Programs/packages/morpho-newlinalg/src/xmatrix.c" "CMakeFiles/newlinalg.dir/src/xmatrix.c.o" "gcc" "CMakeFiles/newlinalg.dir/src/xmatrix.c.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")

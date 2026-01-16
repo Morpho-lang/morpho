cff963e117708d86fefe02ee4b6d9fb334700e4a
Tim Atherton <54725421+softmattertheory@users.noreply.github.com>
2026-01-15 20:50:48 -0500

    Migrate some helper functions for sparse constructor functions

--- src/classes/array.c
+++ src/classes/array.c
@@ -192,8 +192,8 @@ errorid array_error(objectarrayerror err) {
 /** Converts an array error into an matrix error code for use in slices*/
 errorid array_to_matrix_error(objectarrayerror err) {
 #ifdef MORPHO_INCLUDE_LINALG
-    switch (err) {
-        case ARRAY_OUTOFBOUNDS: return MATRIX_INDICESOUTSIDEBOUNDS;
+    switch (err) { 
+        case ARRAY_OUTOFBOUNDS: return LINALG_INDICESOUTSIDEBOUNDS;
         case ARRAY_WRONGDIM: return MATRIX_INVLDNUMINDICES;
         case ARRAY_NONINTINDX: return MATRIX_INVLDINDICES;
         case ARRAY_ALLOC_FAILED: return ERROR_ALLOCATIONFAILED;

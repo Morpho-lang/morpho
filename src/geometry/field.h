de226642caf7b40a7134122797a12eb959ecd12d
softmattertheory <timothy.atherton@tufts.edu>
2026-05-18 16:57:53 +0200

    Fix finiteElementSpace label; better CG3 refinement

--- src/geometry/field.h
+++ src/geometry/field.h
@@ -72,7 +72,7 @@ extern value field_gradeoption;
 
 #define FIELD_OP_METHOD      "op"
 #define FIELD_SHAPE_METHOD   "shape"
-#define FIELD_FESPACE_METHOD   "finiteelementspace"
+#define FIELD_FESPACE_METHOD   "finiteElementSpace"
 #define FIELD_PROTOTYPE_METHOD   "prototype"
 #define FIELD_MESH_METHOD    "mesh"
 #define FIELD_EVALELEMENT_METHOD "evalElement"
